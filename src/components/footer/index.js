d734e7cc183052b7824000c00430339a468347cd
Loma <909622852@163.com>
2019-12-25 15:19:48 +0800

    修改页面

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './style.less';
+
+export default class Footer extends Component {
+    render() {
+        return <div id='chat-footer'>
+            chat-
+        </div>
+    }
+}
---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './style.less';
+
+export default class Header extends Component {
+    render() {
+        return <div id='chat-header'>
+            chat-
+        </div>
+    }
+}
